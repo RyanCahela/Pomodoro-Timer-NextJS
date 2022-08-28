4f638bcda487070eaef4dc409f1acfe3a912e297
RyanCahela <6194542+RyanCahela@users.noreply.github.com>
2022-08-28 00:20:39 -0700

    added sass, timer now used Date() constructor to keep track of time

--- components/TimeDisplay.js
+++ components/TimeDisplay.js
@@ -1,17 +1,16 @@
 import React from "react";
 
-const DisplayTime = ({ miliseconds }) => {
+const TimeDisplay = ({ miliseconds }) => {
   const seconds = Math.floor(miliseconds / 1000);
   const minutes = Math.floor(seconds / 60);
   const hours = Math.floor(minutes / 60);
   return (
-    <div>
+    <div className="time-display">
       <span>{hours}:</span>
       <span>{minutes}:</span>
-      <span>{seconds}:</span>
-      <span>{miliseconds % 100}</span>
+      <span>{seconds}</span>
     </div>
   );
 };
 
-export default DisplayTime;
+export default TimeDisplay;
